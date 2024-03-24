7726c24fe88e0e407f183a89cb1f546bc304884b
Kelvin  Ryan <90951461+KelvinMoreira@users.noreply.github.com>
2024-03-23 21:38:18 -0300

    50% pjt
    projeto metade feito

---
+++ semaforo-action/index.js
@@ -0,0 +1,23 @@
+let vermelho = document.querySelector('.button1')
+let amarelo = document.querySelector('.button2')
+let verde = document.querySelector('button3')
+let automatico = document.querySelector('button4')
+
+function ligarVermelha(){
+    let imagem = document.querySelector('.imageContainer')
+    imagem.src = 'img/vermelho.png'
+}
+
+function ligarAmarelo(){
+    let imagem = document.querySelector('.imageContainer')
+    imagem.src = 'img/amarelo.png'
+}
+
+function ligarVerde(){
+    let imagem = document.querySelector('.imageContainer')
+    imagem.src = 'img/verde.png'
+}
+
+function ligarAutomatico(){
+    
+}
